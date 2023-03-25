78a88b58a56f87dd329c058aba0280a251f89c19
arsenalkingsley <stepupitsreal@gmail.com>
2023-03-24 21:45:57 -0700

    git commit

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,7 @@
 # creates a file in the /tmp directory
 file { '/tmp/school':
-  ensure  => 'file',
+  content => 'i love Puppet',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  content => 'I love Puppet',
-}
+ }
